933d911bb373655ae1de3646bc13696ff5dfed32
FelipeHardmann <fashardmann@gmail.com>
2022-11-18 15:56:10 -0300

    Aula 2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,9 @@
 import './home.css'
 
+import {Social} from '../../components/Social'
+
+import {FaFacebook, FaInstagram, FaYoutube} from 'react-icons/fa'
+
 export default function Home(){
     return(
         <div className='home-container'>
@@ -7,6 +11,7 @@ export default function Home(){
             <span>Veja meus links</span>
 
             <main className='links'>
+                
                 <section className='link-area'>
                     <a href='#'>
                         <p  className='link-text'>Instagram</p>
@@ -24,6 +29,24 @@ export default function Home(){
                         <p  className='link-text'>Instagram</p>
                     </a>
                 </section>
+
+
+
+                <footer>
+                    <Social url='https://facebook.com/sujeitoprogramador'>
+                        <FaFacebook size={35} color='#fff'/>
+                    </Social>
+
+                    <Social url='https://youtube.com/sujeitoprogramador'>
+                        <FaYoutube size={35} color='#fff'/>
+                    </Social>
+
+                    <Social url='https://instagram.com/sujeitoprogramador'>
+                        <FaInstagram size={35} color='#fff'/>
+                    </Social>
+
+                </footer>
+
             </main>
         </div>
     )
